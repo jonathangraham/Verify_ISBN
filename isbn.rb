fea0ff3a1e96767e61b3720b2ea4be26e79885ac
smellydog40 <smellydog@outlook.com>
2015-09-22 18:57:13 -0400

    13 number test working.  Code to run the program added.

--- isbn.rb
+++ isbn.rb
@@ -7,31 +7,82 @@ n = n.to_s
 	n.delete! '-'
 	end
 
+	if n.length == 10
+	check_digit_contains_X(n)
+	elsif n.length == 13
+	check_digit_valid_13(n)
+	else puts "that is not a valid ISBN number."
+	end
+	
 n.length == 10 || n.length == 13
+end
+
+def check_digit_contains_X(n)
+number = n.split ""
+
+	if number[9] == "x" || number[9] == "X"
+	number[9] = 10
+	end
+	
+number[9] == 10
+check_digit_valid_10(number)
 end
 
 def check_digit_valid_10(n)
 array =[]
-number = n.split ""
-	
-	if number[9] == "x" || number[9] == "X"
-	number[9] = 10
-	end
 
-	number.each { |value|
+	n.each { |value|
 	array << value.to_i }
 
 sum = 0
-print array
-puts "\n"
+
 	array.each.with_index { |value, index|
 	break if index == 9
 	sum += (value * (index + 1)) }
 
 check_digit = sum%11
-check_digit == array[9]	
 
+	if check_digit == array[9]
+	puts "This is a valid ISBN number."
+	else puts "This is not a valid ISBN number."
+	end
+	
+check_digit == array[9]
 end
 
-#puts check_digit_valid_10("0306406152")
-puts check_digit_valid_10("!@#$%^&*12")
+def check_digit_valid_13(n)
+number = n.split ""
+array =[]
+
+	number.each { |value|
+	array << value.to_i }
+	
+sum = 0
+check_digit = 0
+	
+        array.each_with_index { |value, index|
+            break if index == 12
+			if index % 2 == 0
+            sum += value * 1
+            else
+            sum += value * 3
+			end 
+			}
+	sum = sum % 10
+    check_digit = (10 - sum)
+	
+		if check_digit == 10
+		check_digit = 0
+		end
+		
+	if array[12] == check_digit
+	puts "This is a valid ISBN number."
+	else puts "This is not a valid ISBN number."
+	end
+
+array[12] == check_digit
+end
+
+puts "Enter the ISBN number to be verified:"
+n = gets.chomp
+valid_isbn(n)
--- isbn_test.rb
+++ isbn_test.rb
@@ -23,9 +23,19 @@ class Isbn_test < Minitest::Test
 	assert_equal(true, valid_isbn("123 4567 890"))
 	end
 	
-	def test_for_10_digit_math
-	assert_equal(true, check_digit_valid_10("0306406152"))
-	assert_equal(true, check_digit_valid_10("08044-2957X"))
+	def test_for_x
+	assert_equal(true, check_digit_contains_X("080442957X"))
 	end
+	
+	def test_for_10_digit_math
+	assert_equal(true, check_digit_valid_10([0,3,0,6,4,0,6,1,5,2]))
+	end
+	
+	def test_for_thirteen_character_math
+    assert_equal(true, check_digit_valid_13("9780156027328"))
+    assert_equal(false, check_digit_valid_13("780670020485"))
+    assert_equal(false, check_digit_valid_13("134567891111"))
+	assert_equal(true, check_digit_valid_13("9780306406157"))
+    end 
 	
 end
