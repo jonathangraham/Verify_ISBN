1906721de273af4b20fcfd4435d02e2daaa6877b
smellydog40 <smellydog@outlook.com>
2015-09-24 21:43:13 -0400

    created function to check if there are any non digits in the number (aftere checking for x in the case of 10 digit numbers.  Rearranged some functions to streamline flow.  Need to overhaul the test file and diagnose why all isbn's entered return invalid.

--- isbn.rb
+++ isbn.rb
@@ -1,20 +1,32 @@
-def valid_isbn(n)
-n = n.to_s
+def check_for_space_or_dash(n)
 
 	if n.include? " "
 	n.delete! ' '
 	elsif n.include? "-"
 	n.delete! '-'
 	end
+check_number_length(n)
+end
 
-	if n.length == 10
+def check_number_length(n)
+
+case n
+	when n.length == 10
 	check_digit_contains_X(n)
-	elsif n.length == 13
-	check_digit_valid_13(n)
-	else puts "that is not a valid ISBN number."
-	end
-	
-n.length == 10 || n.length == 13
+		if only_numeric_characters(check_digit_contains_X) == true
+		output(false)
+		else check_digit_valid_10(check_digit_contains_X)
+		end
+		
+	when n.length == 13
+		if only_numeric_characters(check_digit_contains_X) == true
+		output(false)
+		else check_digit_valid_10(n)
+		end
+		
+	else output(false)
+end
+
 end
 
 def check_digit_contains_X(n)
@@ -23,9 +35,14 @@ number = n.split ""
 	if number[9] == "x" || number[9] == "X"
 	number[9] = 10
 	end
+
+string = number.join("")	
+end
+
+def only_numeric_characters(string)
 	
-number[9] == 10
-check_digit_valid_10(number)
+	true if string =~ /\D/ 
+
 end
 
 def check_digit_valid_10(n)
@@ -41,17 +58,16 @@ sum = 0
 	sum += (value * (index + 1)) }
 
 check_digit = sum%11
-
-	if check_digit == array[9]
-	puts "This is a valid ISBN number."
-	else puts "This is not a valid ISBN number."
-	end
 	
-check_digit == array[9]
+	if check_digit == array[9]
+	output(true)
+	else output(false)
+	end
 end
 
 def check_digit_valid_13(n)
 number = n.split ""
+	
 array =[]
 
 	number.each { |value|
@@ -76,13 +92,19 @@ check_digit = 0
 		end
 		
 	if array[12] == check_digit
-	puts "This is a valid ISBN number."
-	else puts "This is not a valid ISBN number."
+	output(true)
+	else output(false)
 	end
 
-array[12] == check_digit
+end
+
+def output(result)
+	if result == true
+	puts "That is a valid ISBN number."
+	else puts "That is not a valid ISBN number."
+	end
 end
 
 puts "Enter the ISBN number to be verified:"
 n = gets.chomp
-valid_isbn(n)
+check_for_space_or_dash(n)
--- isbn_test.rb
+++ isbn_test.rb
@@ -4,15 +4,15 @@ require "minitest/autorun"
 class Isbn_test < Minitest::Test
 
 	def test_for_correct_number_of_characters_10
-	assert_equal(true, valid_isbn(1234567890))
+	assert_equal(true, valid_isbn("1234567890"))
 	end
 	
 	def test_for_correct_number_of_characters_13
-	assert_equal(true, valid_isbn(1234567890123))
+	assert_equal(true, valid_isbn("1234567890123"))
 	end
 	
 	def test_for_correct_number_of_characters_15
-	assert_equal(false, valid_isbn(123456789012345))
+	assert_equal(false, valid_isbn("123456789012345"))
 	end
 	
 	def test_to_remove_dashes
@@ -26,6 +26,16 @@ class Isbn_test < Minitest::Test
 	def test_for_x
 	assert_equal(true, check_digit_contains_X("080442957X"))
 	end
+	
+	def test_for_only_numeric_characters
+	assert_equal(nil, only_numeric_characters("1234567890"))
+	assert_equal(true, only_numeric_characters("123x4567890"))
+	end
+	
+	#def check_that_all_are_numbers
+	#assert_equal(true, check_that_all_are_numbers(["1","2","3","4","5","6","7","8","9","0"]))
+	#assert_equal(false, check_that_all_are_numbers(["1","2","3","x","5","6","7","8","9","0"]))
+	#end
 	
 	def test_for_10_digit_math
 	assert_equal(true, check_digit_valid_10([0,3,0,6,4,0,6,1,5,2]))
